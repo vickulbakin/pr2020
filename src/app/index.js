9951a1b9191d43b6e421fdc7db173355c1985003
vickulbakin <vickulbakin@mail.ru>
2019-11-28 11:02:11 +0300

    feat: first webpack config

---
+++ src/app/index.js
@@ -0,0 +1,9 @@
+import '../style/app.scss';
+
+class TestClass {
+    constructor() {
+        let msg = "Using ES2015+ syntax";
+        console.log(msg);
+    }
+}
+let test = new TestClass();
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+module.exports = {
+  entry: __dirname + "/src/app/index.js",
+  output: {
+    path: __dirname + "/dist",
+    filename: "bundle.js",
+    publicPath: "/"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: "babel-loader",
+        exclude: [/node_modules/]
+      },
+      {
+        test: /\.(sass|scss)$/,
+        use: [{
+            loader: "style-loader" // creates style nodes from JS strings
+        }, {
+            loader: "css-loader" // translates CSS into CommonJS
+        }, {
+            loader: "sass-loader" // compiles Sass to CSS
+        }]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: __dirname + "/src/public/index.html",
+      inject: "body"
+    })
+  ],
+  devServer: {
+    contentBase: "./src/public",
+    port: 7700
+  }
+};
