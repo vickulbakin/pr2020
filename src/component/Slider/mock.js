1085a196d765eb5c7ea530cc44a4f86ff1eec00d
vickulbakin <vickulbakin@mail.ru>
2020-01-28 22:07:12 +0300

    add presentation components

---
+++ src/component/Slider/mock.js
@@ -0,0 +1,22 @@
+export const slides = [
+  {
+    "title": "Слайд 1",
+    "description": "Описание к слайду 1",
+  },
+  {
+    "title": "Слайд 2",
+    "description": "Описание к слайду 2",
+  },
+  {
+    "title": "Слайд 3",
+    "description": "Описание к слайду 3",
+  },
+  {
+    "title": "Слайд 4",
+    "description": "Описание к слайду 4",
+  },
+  {
+    "title": "Слайд 5",
+    "description": "Описание к слайду 5",
+  }
+];
