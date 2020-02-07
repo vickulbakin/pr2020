526dcfcff1ed1ce8c9f1c055c97a19895658666a
vickulbakin <vickulbakin@mail.ru>
2020-02-07 21:01:10 +0300

    add clean-webpack-plugin, file-loader, html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,14 @@
 const path = require("path");
 const postcssPresetEnv = require("postcss-preset-env");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: "./src/index.js",
   mode: "development",
   output: {
-    filename: "./main.js"
+    path: path.resolve(__dirname, './dist'),
+    filename: 'main.js'
   },
   devServer: {
     contentBase: path.join(__dirname, "dist"),
@@ -55,11 +58,35 @@ module.exports = {
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
-        use: ["file-loader"]
-      }
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath: 'images',
+            },
+          }
+        ],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath: 'fonts',
+            },
+          },
+        ],
+      },
     ]
   },
   resolve: {
     extensions: [".js", ".jsx"]
-  }
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
 };
